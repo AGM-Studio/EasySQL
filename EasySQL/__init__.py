15781c4c9e195711749fffff3f333b1f314e30ed
Ashenguard <myminecraft2019@gmail.com>
2026-05-14 00:13:57 +0330

    Added Quick Database

--- EasySQL/__init__.py
+++ EasySQL/__init__.py
@@ -10,3 +10,18 @@ from .Decorators import auto_init
 from . import EasyInstances
 from . import Types
 from . import Characters as Charsets
+
+
+def quick_database(name: str, host: str = "127.0.0.1", port: int = 3306, user: str = "root", password: str = None, charset: CHARSET = None):
+    class QuickDatabase(EasyDatabase):
+        _database: str = name
+        _password: str = password
+        _host: str = host
+        _port: int = port
+        _user: str = user
+
+        _charset: CHARSET = charset
+
+    database = QuickDatabase()
+    database.__class = QuickDatabase
+    return database
